a35e2e95647da456d471c57c04712b5962718116
PolumnaLovegood <bashanov_a@iuca.kg>
2020-11-03 20:41:15 +0600

    second commit

--- mysite/contacts/models.py
+++ mysite/contacts/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Contact(models.Model):
-    last_name = models.CharField(max_length=254, verbose_name='Фамилия')
-    first_name = models.CharField(max_length=254, verbose_name='Имя')
-    patronymic = models.CharField(max_length=254, null=True, blank=True, verbose_name='Отчество')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    patronymic = models.CharField(max_length=50, null=True, blank=True, verbose_name='Отчество')
     date_of_birth = models.DateField(verbose_name='Дата рождения')
     phone = models.CharField(max_length=250, verbose_name='Номер телефона')
     email = models.EmailField(max_length=100, null=True, blank=True, verbose_name='Email')
-    social_page_networks = models.CharField(max_length=254, null=True, blank=True, verbose_name='Страницы в соц. сетях')
+    social_page_networks = models.CharField(max_length=25, null=True, blank=True, verbose_name='Страницы в соц. сетях')
     deck = models.TextField(max_length=250, default='Подозрительная личность', verbose_name='Описание')
 
     def __str__(self):
